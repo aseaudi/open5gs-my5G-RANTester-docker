10a813de7e897c5b55c33ff055dba9356dfdbcdd
Abdelmuhaimen Seaudi <aseaudi@gmail.com>
2023-09-26 08:59:51 +0000

    1

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-cd
-cd open5gs-my5G-RANTester-docker/nf_tester
-docker build . -t my5g:msin-offset --no-cache
-cd
-cd new/open5gs-my5G-RANTester-docker
+cd nf_tester
+docker build . -t my5g:pdu-release --no-cache
+cd ..
 
